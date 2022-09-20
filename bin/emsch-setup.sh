87fe548ca4dddae0633a63033ed2d1a1fecb5267
mdk <mathieu.dekeyzer@smals.be>
2022-09-20 17:54:01 +0200

    feat: SAMEORIGIN

--- bin/emsch-setup.sh
+++ bin/emsch-setup.sh
@@ -113,7 +113,7 @@ EOL
     ErrorLog /dev/stderr
     CustomLog /dev/stdout common
     Header set Cache-Control "${APACHE_CACHE_CONTROL:-"max-age=86400, public"}"
-    Header always append X-Frame-Options "${APACHE_X_FRAME_OPTIONS:-"DENY"}"
+    Header always append X-Frame-Options "${APACHE_X_FRAME_OPTIONS:-"SAMEORIGIN"}"
     Header always append X-XSS-Protection "${APACHE_X_XSS_PROTECTION:-"1"}"
     Header always append X-Content-Type-Options "${APACHE_X_CONTENT_TYPE_OPTIONS:-"nosniff"}"
     RewriteEngine On
