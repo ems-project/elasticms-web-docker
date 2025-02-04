0ad73c9db0d716f06212b8ef34c8f1088d837ace
Sébastian Molle <sebastian.molle@gmail.com>
2025-02-04 12:31:00 +0100

    Reuse environment variable if define at deployment scope

--- bin/container-entrypoint.d/entrypoint.d/10-folders.sh
+++ bin/container-entrypoint.d/entrypoint.d/10-folders.sh
@@ -5,7 +5,8 @@ export APP_SRC_DIR="/app/src/elasticms"
 export APP_TMP_DIR="${TMPDIR}"
 
 export APP_CONFIG_DIR="${APP_TMP_DIR}/elasticms.d"
-export APP_CACHE_DIR="/app/var/cache/elasticms"
 export APP_LOG_DIR="/app/var/log/elasticms"
 
+APP_CACHE_DIR_DEFAULT="/app/var/cache/elasticms"
+export APP_CACHE_DIR=${APP_CACHE_DIR:-"${APP_CACHE_DIR_DEFAULT}"}
 true
